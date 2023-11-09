93c53f1f44a5af852ef5b2aa1a0f9511ded29c9a
Marcelo Fabiano <marcelofabianov@gmail.com>
2023-11-09 15:26:44 -0300

    chore: configurando prisma swagger nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 import configuration from './config/configuration';
 
 @Module({
@@ -9,6 +10,7 @@ import configuration from './config/configuration';
     ConfigModule.forRoot({
       load: [configuration],
     }),
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
